4ac7df542297c3366f89259ea942c8c09bdf4258
David Garcia <josdabus.gd5154@gmail.com>
2024-06-12 12:16:09 -0500

    bd xd

--- BackEnd/configDB.js
+++ BackEnd/configDB.js
@@ -2,11 +2,7 @@ const config = {
     user: 'postgres',
     host: 'localhost',
     password: 'root',
-<<<<<<< HEAD
-    database: 'usuarios'
-=======
-    database: 'AppElecciones'
->>>>>>> a8c1bbf33e949d459e10006c37990a9b27249f95
+    database: 'usuarios',
   };
   
   module.exports = config;
